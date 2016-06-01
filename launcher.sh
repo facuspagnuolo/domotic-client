08e00d6fda3cc7e74304481c2fa022baff5b2c5c
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2016-06-01 02:50:53 +0000

    Fix launcher

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd /home/pi/Desktop/domotic-raspberry-client
+cd /home/pi/Desktop/domotic-client
 sudo python reset_gpios.py 
 sudo stdbuf -oL python rpi_client.py > logs/rpi_client.log
 
