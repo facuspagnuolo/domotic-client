a2be7bf6a37cd36a478e68133462f3c9508b85df
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2016-06-05 23:04:41 +0000

    Read analog luminosity and humidity values

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
 cd /home/pi/Desktop/domotic-client
-sudo python reset_gpios.py 
 sudo stdbuf -oL python rpi_client.py > logs/rpi_client.log
-
--- learning/analog/analog_soil_humidity.py
+++ learning/analog/analog_soil_humidity.py
@@ -8,9 +8,9 @@ spi = spidev.SpiDev()
 spi.open(0,0)
 
 # Define constants
-channel = 0
+channel = 5
 delay = 2
- 
+
 # Function to read SPI data from MCP3008 chip
 # Channel must be an integer 0-7
 def ReadChannel(channel):
@@ -24,9 +24,16 @@ def ConvertVolts(data, places):
   volts = round(volts, places)
   return volts
  
+# Function to calculate humidity, rounded to specified number of decimal places.
+def ConvertHumidity(volts, places):
+  humidity = (volts - 3.3) / float(-2.3 / float(950))
+  humidity = round(humidity, places)
+  return humidity
+ 
 while True:
-  luminosity = ReadChannel(channel)
-  volts = ConvertVolts(luminosity, 2)
+  channel_input = ReadChannel(channel)
+  volts = ConvertVolts(channel_input, 2)
+  humidity = ConvertHumidity(volts, 2)
   print "--------------------------------------------"
-  print("Light: {} ({}V)".format(luminosity,volts))
+  print("Humidity: {} ({}V)".format(humidity, volts))
   time.sleep(delay)
